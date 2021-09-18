e0ea9103aeccc66b5b9ed40e02d6741e78ebb4df
DST-QA <90075006+DST-QA@users.noreply.github.com>
2021-09-18 16:45:02 -0600

    Add files via upload

--- unsuccessful.cs
+++ unsuccessful.cs
@@ -1,2 +1,11 @@
-Console.WriteLine("Hello World!");
+string greeting = "      Hello World!       ";
+Console.WriteLine($"[{greeting}]");
 
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
